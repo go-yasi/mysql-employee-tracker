2b041bf5ce1c18717b2358f1395b81c1105f5c19
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-06-24 20:34:05 -0700

    Added a function to read department table info

--- index.js
+++ index.js
@@ -37,115 +37,121 @@ const employeeOpt = () => {
     })
 };
 
-// const addDept = () => {
-//     // console.log('This function works!');
-//     inquirer
-//     .prompt({
-//         name: 'newDept',
-//         type: 'input',
-//         message: 'Enter new department name',
-//     })
-//     .then((answer) => {
-//         connection.query(
-//             'INSERT INTO departments SET ?',
-//         {
-//             department_name: answer.newDept,//what the user typed in
-//         },
-//         (err) => {
-//             if (err) throw err;
-//             console.log('Your department was created successfully!');
-//             employeeOpt(); 
-//         }
-//         );
-//     })
-// };
+const addDept = () => {
+    inquirer
+    .prompt({
+        name: 'newDept',
+        type: 'input',
+        message: 'Enter new department name',
+    })
+    .then((answer) => {
+        connection.query(
+            'INSERT INTO departments SET ?',
+        {
+            department_name: answer.newDept,
+        },
+        (err) => {
+            if (err) throw err;
+            console.log('Your department was created successfully!');
+            employeeOpt(); 
+        }
+        );
+    })
+};
 
-// const addRole = () =>{
-//     inquirer
-//     .prompt([
-//         {
-//             name: 'newRole',
-//             type: 'input',
-//             message: 'Enter new role title',
-//         },
-//         {
-//             name: 'salary',
-//             type: 'input',
-//             message: 'Enter Salary',
-//         },
-//         {
-//             name: 'dptId',
-//             type: 'input',
-//             message: 'Enter Department ID',
-//         }
-//     ])
-//     .then((answer) => {
-//         connection.query(
-//             'INSERT INTO roles SET ?',//
-//             {
-//             title: answer.newRole,
-//             salary: answer.salary,
-//             department_id: answer.dptId,
-//             },
-//             (err) => {
-//                 if (err) throw err;
-//                 console.log('Your auction was created successfully!');
-//                 employeeOpt();
-//             }
-//             );
-//     });
-// };
-
-
-// const addEmp = () => {
-//     inquirer
-//     .prompt([
-//         {
-//         name: 'firstname',
-//         type: 'input',
-//         message: 'Enter employee first name',
-//         },
-//         {
-//         name: 'lastname',
-//         type: 'input',
-//         message: 'Enter employee last name',
-//         },
-//         {
-//         name: 'role',//
-//         type: 'input',
-//         message: 'roleid',//
-//         },
-//         {
-//         name: 'manid',
-//         type: 'input',
-//         message: 'managerid',//
-//         }
-//     ])
-//     .then((answer) => {
-//         connection.query(
-//             'INSERT INTO employees SET ?', //
-//             {
-//                 first_name: answer.firstname,
-//                 last_name: answer.lastname,
-//                 role_id: answer.role,
-//                 manager_id: answer.manid,
-//             },
-//             (err) => {
-//                 if (err) throw err;
-//                 console.log('Employee added successfully!');
-//                 employeeOpt();
-//             }
-//         );
-//     });
-// }
+const addRole = () =>{
+    inquirer
+    .prompt([
+        {
+            name: 'newRole',
+            type: 'input',
+            message: 'Enter new role title',
+        },
+        {
+            name: 'salary',
+            type: 'input',
+            message: 'Enter Salary',
+        },
+        {
+            name: 'dptId',
+            type: 'input',
+            message: 'Enter Department ID',
+        }
+    ])
+    .then((answer) => {
+        connection.query(
+            'INSERT INTO roles SET ?',//
+            {
+            title: answer.newRole,
+            salary: answer.salary,
+            department_id: answer.dptId,
+            },
+            (err) => {
+                if (err) throw err;
+                console.log('Your auction was created successfully!');
+                employeeOpt();
+            }
+            );
+    });
+};
 
 
+const addEmp = () => {
+    inquirer
+    .prompt([
+        {
+        name: 'firstname',
+        type: 'input',
+        message: 'Enter employee first name',
+        },
+        {
+        name: 'lastname',
+        type: 'input',
+        message: 'Enter employee last name',
+        },
+        {
+        name: 'role',//
+        type: 'input',
+        message: 'roleid',//
+        },
+        {
+        name: 'manid',
+        type: 'input',
+        message: 'managerid',//
+        }
+    ])
+    .then((answer) => {
+        connection.query(
+            'INSERT INTO employees SET ?', //
+            {
+                first_name: answer.firstname,
+                last_name: answer.lastname,
+                role_id: answer.role,
+                manager_id: answer.manid,
+            },
+            (err) => {
+                if (err) throw err;
+                console.log('Employee added successfully!');
+                employeeOpt();
+            }
+        );
+    });
+}
 
 
+const viewDepartment = () => {
+    console.log('Pulling up department info...\n');
+    connection.query('SELECT * FROM departments', (err, res) => {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      console.log(res);
+    });
+  };
 
+
+employeeOpt();
 
 connection.connect((err) => {
     if (err) throw err;
-    console.log(`Now listening on PORT ${connection.port}`);
-    connection.end();
+    console.log(`Now listening`);
   });
